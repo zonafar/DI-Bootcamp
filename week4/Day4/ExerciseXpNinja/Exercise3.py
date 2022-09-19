2742701ff18c4dfb8d6ae3ffd2706e182422b376
zona <zonafar2000@gmail.com>
2022-09-19 11:51:57 +0000

    Xp Ninja complete

---
+++ week4/Day4/ExerciseXpNinja/Exercise3.py
@@ -0,0 +1,14 @@
+###########################################
+####  Exercise 3 : Box Of Stars
+###########################################
+# Instructions
+# Write a function named box_printer that takes any amount of strings (not in a list) and prints them, one per line, in a rectangular frame.
+# For example calling box_printer("Hello", "World", "in", "reallylongword", "a", "frame") will result as:
+# ******************
+# * Hello          *
+# * World          *
+# * in             *
+# * reallylongword *
+# * a              *
+# * frame          *
+# ******************
