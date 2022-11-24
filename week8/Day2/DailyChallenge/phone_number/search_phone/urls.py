2f65104ba6ad192c0ff1b2d3bf15b2e1f0a17d28
zona <zonafar2000@gmail.com>
2022-11-24 14:23:33 +0000

    W8D4 daily challenge

--- week8/Day2/DailyChallenge/phone_number/search_phone/urls.py
+++ week8/Day2/DailyChallenge/phone_number/search_phone/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('phonenumber/<str:phone>/',views.phonenumber,name='phonenumber' ),
     path('name/<str:name>/',views.name,name='name'),
+    path('search/',views.person,name='person'),
 ]
